da065aee05d76d3fc3d0e83bfcdde1697227f50c
Carlos Matos <los.9806@gmail.com>
2024-10-19 16:45:59 -0500

    Area of a rectangle Assignment

---
+++ areaOfRectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prompt the user for the length of the rectangle
+Console.WriteLine("Enter the length a the rectangle: ");
+
+// Store the user's response
+double length = Convert.ToDouble(Console.ReadLine());
+
+// Prompt the user for the width of the rectangle
+Console.WriteLine("Enter the width a the rectangle: ");
+
+// Store the user's response
+double width = Convert.ToDouble(Console.ReadLine());
+
+// Calculate the area of the rectangle
+double area = length * width;
+
+// Print the result using concatenation
+Console.WriteLine("The area of the rectangle with the length of " + length + " and the width of " + width +  " is: " + area);
